24e18f187294b4742b0071a755971ba30ba5f1cb
tanjib10 <radiulmd@gmail.com>
2023-11-19 14:35:46 +0600

    Project setup done

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Root/Root.jsx";
+import ErrorPage from "./Root/ErrorPage.jsx";
+import Home from "./Home/Home.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
